87e22d594585f9483af2d3187a663f74c14bb884
Newemailop <newemailop779@gmail.com>
2024-05-20 19:55:03 +0530

    Update datas.py

--- Hellbot/functions/datas.py
+++ Hellbot/functions/datas.py
@@ -1,28 +1,4 @@
-#MIT License
-
-#Copyright (c) 2024 Japanese-X-Userbot
-
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
-
-#kexx (⌯ Kᴜɴᴀʟ࿐ 🕊)
-
-MASTERS = [6694740726]
+MASTERS = [7015000049]
 
 RAID = [
 "𝐦𝐚𝐝𝐚𝐫𝐜𝐡𝐨𝐝 𝐭𝐞𝐫𝐢 𝐦𝐚𝐚 𝐤𝐢 𝐜𝐡𝐮𝐭 𝐦𝐞 𝐠𝐡𝐮𝐭𝐤𝐚 𝐤𝐡𝐚𝐚𝐤𝐞 𝐭𝐡𝐨𝐨𝐤 𝐝𝐮𝐧𝐠𝐚 🤣🤣",
