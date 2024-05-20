4cb1babd7f7acf503332a837ab5836cb4d8ec6fc
Newemailop <newemailop779@gmail.com>
2024-05-20 20:00:01 +0530

    Update abuse.py

--- Hellbot/plugins/user/abuse.py
+++ Hellbot/plugins/user/abuse.py
@@ -1,44 +1,14 @@
-#MIT License
-
-#Copyright (c) 2024 Japanese-X-Userbot
-
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
-
-# Credits: KUNAL AND NOBITA XD 
-# Copyright (C) 2024 JAPANESE X USERBOT AND STORM USERBOT 
-#DON'T KANG FUCKING COWARD
-#BSDKE KANG KIYA TOH SOCH LIYO
-#AAG LAGA DUNGA TERE ANDAR 
-#SAMJHA ? 
-
 from random import choice
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from XDB.data import MASTERS, ABUSE
-from config import OWNER_ID
-from config import SUDO_USERS
-from config import CMD_HANDLER as cmd
+from functions.datas import MASTERS, ABUSE
+from Hellbot.core.config import Config
+from Config import HANDLER as cmd
 from .help import *
 import asyncio
 
 @Client.on_message(
-    filters.command(["abuse"], ".") & (filters.me | filters.user(SUDO_USERS))
+    filters.command(["abuse"], ".") & (filters.me | filters.user(STAN))
 )
 async def abuse(x: Client, e: Message):
     NOBI = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
